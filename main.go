587c70832c3f6351a7ed6ea53531b8c6af82f8d6
potproject <pptppctt@gmail.com>
2021-11-12 16:04:27 +0900

    add main

--- main.go
+++ main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/potproject/goenvgen/gen"
 )
 
+var (
+	pkg   = flag.String("p", "envgen", "the name of the package for the generated code")
+	types = flag.String("t", "", "Manually type definition setting (example: \"-t ENV_BOOL=bool,ENV_S_INT=[]int\")")
+)
+
 func main() {
-	err := gen.GenerateFile("test/.env.test", "envgen", map[string]string{})
+	flag.Parse()
+	err := run(flag.Args())
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
 	}
+	os.Exit(0)
+}
+
+func run(args []string) error {
+	file := ".env"
+	if len(args) == 1 {
+		file = args[0]
+	}
+	if len(args) > 1 {
+		return errors.New(".env File is not specified")
+	}
+	mapmtds := map[string]string{}
+	if *types != "" {
+		mtds := strings.Split(*types, ",")
+		for _, v := range mtds {
+			mtd := strings.Split(v, "=")
+			if len(mtd) != 2 {
+				return errors.New("Manually type definition invalid value: " + v)
+			}
+			mapmtds[mtd[0]] = mtd[1]
+		}
+	}
+	err := gen.GenerateFile(file, *pkg, mapmtds)
+	return err
 }
