085379ebdc45354921f96c9834520cfe4a3c6aa9
potproject <pptppctt@gmail.com>
2021-11-12 13:03:32 +0900

    add t

--- test/envgentest/envgentest_test.go
+++ test/envgentest/envgentest_test.go
@@ -9,6 +9,139 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func Test_EnvGenTestSet(t *testing.T) {
+	Set().BOOL_TEST1(true)
+	if true != Get().BOOL_TEST1() {
+		t.Errorf("BOOL_TEST1 invalid")
+	}
+	Set().BOOL_TEST2(false)
+	if false != Get().BOOL_TEST2() {
+		t.Errorf("BOOL_TEST2 invalid")
+	}
+	Set().BOOL_TEST3(true)
+	if true != Get().BOOL_TEST3() {
+		t.Errorf("BOOL_TEST2 invalid")
+	}
+
+	Set().INT64_TEST1(0)
+	if 0 != Get().INT64_TEST1() {
+		t.Errorf("INT64_TEST1 invalid")
+	}
+	Set().INT64_TEST2(-9223372036854775808)
+	if -9223372036854775808 != Get().INT64_TEST2() {
+		t.Errorf("INT64_TEST2 invalid")
+	}
+	Set().INT64_TEST3(9223372036854775807)
+	if 9223372036854775807 != Get().INT64_TEST3() {
+		t.Errorf("INT64_TEST3 invalid")
+	}
+	Set().FLOAT64_TEST1(9223372036854775808)
+	if 9223372036854775808 != Get().FLOAT64_TEST1() {
+		t.Errorf("FLOAT64_TEST1 invalid")
+	}
+	Set().FLOAT64_TEST2(0.1)
+	if 0.1 != Get().FLOAT64_TEST2() {
+		t.Errorf("FLOAT64_TEST2 invalid")
+	}
+	Set().FLOAT64_TEST3(-0.1234567890)
+	if -0.1234567890 != Get().FLOAT64_TEST3() {
+		t.Errorf("FLOAT64_TEST3 invalid")
+	}
+	Set().STRING_TEST1("")
+	if "" != Get().STRING_TEST1() {
+		t.Errorf("STRING_TEST1 invalid")
+	}
+	Set().STRING_TEST2("-")
+	if "-" != Get().STRING_TEST2() {
+		t.Errorf("FLOAT64_TEST2 invalid")
+	}
+	Set().STRING_TEST3("100m")
+	if "100m" != Get().STRING_TEST3() {
+		t.Errorf("STRING_TEST3 invalid")
+	}
+	Set().Lowercasetest("lowercasetest")
+	if "lowercasetest" != Get().Lowercasetest() {
+		t.Errorf("lowercasetest invalid")
+	}
+	Set().UPPERCASETEST("UPPERCASETEST")
+	if "UPPERCASETEST" != Get().UPPERCASETEST() {
+		t.Errorf("UPPERCASETEST invalid")
+	}
+	Set().CamelCaseTest("CamelCaseTest")
+	if "CamelCaseTest" != Get().CamelCaseTest() {
+		t.Errorf("CamelCaseTest invalid")
+	}
+	Set().Lower_snake_case_test("lower_snake_case_test")
+	if "lower_snake_case_test" != Get().Lower_snake_case_test() {
+		t.Errorf("lower_snake_case_test invalid")
+	}
+	Set().INVALID_JSON_TEST("{\"\"}")
+	if "{\"\"}" != Get().INVALID_JSON_TEST() {
+		t.Errorf("INVALID_JSON_TEST invalid")
+	}
+	Set().UNDERSCORE_TEST("underscore_test")
+	if "underscore_test" != Get().UNDERSCORE_TEST() {
+		t.Errorf("_TEST invalid")
+	}
+	Set().S_STRING_TEST([]string{"", ""})
+	if diff := cmp.Diff([]string{"", ""}, Get().S_STRING_TEST()); diff != "" {
+		t.Errorf("S_STRING_TEST invalid: %s", diff)
+	}
+	Set().S_STRING_TEST2([]string{"true", "100", "abc"})
+	if diff := cmp.Diff([]string{"true", "100", "abc"}, Get().S_STRING_TEST2()); diff != "" {
+		t.Errorf("S_STRING_TEST2 invalid: %s", diff)
+	}
+	Set().S_INT64_TEST([]int64{1, 10, 100, 1000})
+	if diff := cmp.Diff([]int64{1, 10, 100, 1000}, Get().S_INT64_TEST()); diff != "" {
+		t.Errorf("S_INT64_TEST invalid: %s", diff)
+	}
+	Set().S_FLOAT64_TEST([]float64{0.1, 0.01, 0.001, 0.0001})
+	if diff := cmp.Diff([]float64{0.1, 0.01, 0.001, 0.0001}, Get().S_FLOAT64_TEST()); diff != "" {
+		t.Errorf("S_FLOAT64_TEST invalid: %s", diff)
+	}
+	Set().S_BOOL_TEST([]bool{true, false, false})
+	if diff := cmp.Diff([]bool{true, false, false}, Get().S_BOOL_TEST()); diff != "" {
+		t.Errorf("S_BOOL_TEST invalid: %s", diff)
+	}
+	Set().JSON_TEST(JSON_TEST{})
+	if diff := cmp.Diff(JSON_TEST{}, Get().JSON_TEST()); diff != "" {
+		t.Errorf("JSON_TEST invalid: %s", diff)
+	}
+	Set().JSON_TEST2(JSON_TEST2{Name: "Test", R: 1.23456789, ID: 1})
+	if diff := cmp.Diff(JSON_TEST2{Name: "Test", R: 1.23456789, ID: 1}, Get().JSON_TEST2()); diff != "" {
+		t.Errorf("JSON_TEST2 invalid: %s", diff)
+	}
+	JSON_TEST3_VAR := JSON_TEST3{
+		Category: struct {
+			ID       int64  "json:\"ID\""
+			Name     string "json:\"Name\""
+			Products []struct {
+				Name string "json:\"Name\""
+				ID   string "json:\"_id\""
+			} "json:\"Products\""
+			Tags []float64 "json:\"Tags\""
+		}{
+			ID:   10,
+			Name: "Test",
+			Products: []struct {
+				Name string "json:\"Name\""
+				ID   string "json:\"_id\""
+			}{{Name: "Test100", ID: "XV"}},
+			Tags: []float64{0.1, 0.01, 0.001},
+		},
+		R:  []float64{1.23456789},
+		ID: 1,
+	}
+	Set().JSON_TEST3(JSON_TEST3_VAR)
+	if diff := cmp.Diff(JSON_TEST3_VAR, Get().JSON_TEST3()); diff != "" {
+		t.Errorf("JSON_TEST3 invalid: %s", diff)
+	}
+	Set().Lower_snake_case_test_json(Lower_snake_case_test_json{Ids: []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}})
+	if diff := cmp.Diff(Lower_snake_case_test_json{Ids: []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}, Get().Lower_snake_case_test_json()); diff != "" {
+		t.Errorf("lower_snake_case_test_json invalid: %s", diff)
+	}
+}
+
 func Test_EnvGenTestGet(t *testing.T) {
 	dir, _ := os.Getwd()
 	testfile := filepath.Join(dir, "..", "..", "test", ".env.test")
@@ -70,6 +203,9 @@ func Test_EnvGenTestGet(t *testing.T) {
 	}
 	if "underscore_test" != Get().UNDERSCORE_TEST() {
 		t.Errorf("_TEST invalid")
+	}
+	if diff := cmp.Diff([]string{"", ""}, Get().S_STRING_TEST()); diff != "" {
+		t.Errorf("S_STRING_TEST invalid: %s", diff)
 	}
 	if diff := cmp.Diff([]string{"true", "100", "abc"}, Get().S_STRING_TEST2()); diff != "" {
 		t.Errorf("S_STRING_TEST2 invalid: %s", diff)
@@ -202,5 +338,19 @@ func Test_envgentestGetForce(t *testing.T) {
 	}
 	if diff := cmp.Diff([]interface{}{"inter", "face"}, Get().FORCE_S_INTERFACE()); diff != "" {
 		t.Errorf("FORCE_S_INTERFACE invalid: %s", diff)
+	}
+}
+
+func Test_EnvGenTestReset(t *testing.T) {
+	dir, _ := os.Getwd()
+	testfile := filepath.Join(dir, "..", "..", "test", ".env.test")
+	godotenv.Load(testfile)
+	err := Load()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	Reset()
+	if false != Get().BOOL_TEST1() {
+		t.Errorf("Reset Test Error")
 	}
 }
